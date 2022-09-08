5d05e6c7fac0d9c8deaa4747a4f3f9828feab493
zjpedu <jpzhu.nju@smail.nju.edu.cn>
2022-09-08 16:26:13 +0800

    add gdb crash demo

---
+++ lab1/test2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char * buf;
+
+int sum_to_n(int num)
+{
+	int i,sum=0;
+	for(i=1;i<=num;i++)
+		sum+=i;
+	return sum;
+}
+
+void printSum()
+{
+	char line[10];
+	printf("enter a number:\n");
+	fgets(line, 10, stdin);
+	if(line != NULL)
+		strtok(line, "\n");
+	sprintf(buf,"sum=%d",sum_to_n(atoi(line)));
+	printf("%s\n",buf);
+}
+
+int main(void)
+{
+	printSum();
+	return 0;
+}
