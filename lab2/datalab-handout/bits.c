73bb13c02db3d9dc7fef2b874176706ecf6b5a3f
GentleCold <1952173800@qq.com>
2022-09-19 08:24:36 +0800

    change from local

--- lab2/datalab-handout/bits.c
+++ lab2/datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <GentleCold>
+ * < GentleCold >
  *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
