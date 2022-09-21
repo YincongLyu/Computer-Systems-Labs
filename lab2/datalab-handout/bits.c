fb8f5cb1a92b3b4c5d61cf288648735685329af0
XiaoNiaoYou <111627036+TakanashiXny@users.noreply.github.com>
2022-09-21 20:52:43 +0800

    Update bits.c

--- lab2/datalab-handout/bits.c
+++ lab2/datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ~(~x&~y) & ~(x&y);
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -153,7 +153,7 @@ int bitXor(int x, int y) {
  */
 int tmin(void) {
 
-  return 2;
+  return 1<<31;
 
 }
 //2
@@ -165,7 +165,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  return (!(!(x+1)))&(!(((x+1)^x)+1));
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +176,7 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  return !((x&0xAAAAAAAA)+(~0xAAAAAAAA+1));
 }
 /* 
  * negate - return -x 
@@ -186,7 +186,7 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return (~x)+1;
 }
 //3
 /* 
@@ -199,7 +199,7 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  return (!((x+(~0x30+1))&(1<<31)))&(!((0x39+(~x+1))&(1<<31)));
 }
 /* 
  * conditional - same as x ? y : z 
