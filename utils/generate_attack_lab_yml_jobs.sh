3eab71edc9bfbdc65a47c2ecab6aec23168a7960
zjpedu <jpzhu.nju@smail.nju.edu.cn>
2022-10-23 21:39:45 +0800

    modify pipeline

--- utils/generate_attack_lab_yml_jobs.sh
+++ utils/generate_attack_lab_yml_jobs.sh
@@ -1,23 +1,33 @@
+echo "name: Attack Lab CI"
+
+echo "on:"
+echo "  push:"
+echo "  pull_request:"
+echo "env:"
+echo '  GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}'
+
+echo "jobs:"
+
 for i in `seq 2`
 do
-        echo "job-4_$i:"
-        echo "  runs-on: ubuntu-latest"
-        echo "  steps:"
-        echo "  - uses: actions/checkout@v2"
-        echo "  - name: Execute Codes"
-        echo "    run: |"
-        echo '      ddl=`date -d "2022-11-06 15:59" +%s --utc`'
-        echo '      current_time=`date +%s`'
-        echo '      [ $current_time -le $ddl ]'
-        echo "      set -x"
-        echo "      cd ./lab4/attack-handout"
-        echo "      cd ./$i"
-        echo "      cat answer1.txt | ./hex2raw | ./ctarget -q | grep "PASS" | wc -l"
-        echo '      [ $? -eq 2 ]'
-        echo "      cat answer2.txt | ./hex2raw | ./ctarget -q | grep "PASS" | wc -l"
-        echo '      [ $? -eq 2 ]'
-        echo "      cat answer3.txt | ./hex2raw | ./ctarget -q | grep "PASS" | wc -l"
-        echo '      [ $? -eq 2 ]'
-        echo "      cat answer4.txt | ./hex2raw | ./rtarget -q | grep "PASS" | wc -l"
-        echo '      [ $? -eq 2 ]'
+        echo "  job-4_$i:"
+        echo "    runs-on: ubuntu-latest"
+        echo "    steps:"
+        echo "    - uses: actions/checkout@v2"
+        echo "    - name: Execute Codes"
+        echo "      run: |"
+        echo '        ddl=`date -d "2022-11-06 15:59" +%s --utc`'
+        echo '        current_time=`date +%s`'
+        echo '        [ $current_time -le $ddl ]'
+        echo "        set -x"
+        echo "        cd ./lab4/attack-handout"
+        echo "        cd ./$i"
+        echo "        cat answer1.txt | ./hex2raw | ./ctarget -q | grep "PASS" | wc -l"
+        echo '        [ $? -eq 2 ]'
+        echo "        cat answer2.txt | ./hex2raw | ./ctarget -q | grep "PASS" | wc -l"
+        echo '        [ $? -eq 2 ]'
+        echo "        cat answer3.txt | ./hex2raw | ./ctarget -q | grep "PASS" | wc -l"
+        echo '        [ $? -eq 2 ]'
+        echo "        cat answer4.txt | ./hex2raw | ./rtarget -q | grep "PASS" | wc -l"
+        echo '        [ $? -eq 2 ]'
 done
